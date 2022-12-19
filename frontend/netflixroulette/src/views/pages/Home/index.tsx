1306f560343da500830e54aa8db9c41aaff327e1
Richard Orofeo <richard_orofeo@epam.com>
2022-12-19 15:37:44 +0800

    added sorting feature by release date

--- frontend/netflixroulette/src/views/pages/Home/index.tsx
+++ frontend/netflixroulette/src/views/pages/Home/index.tsx
@@ -7,35 +7,66 @@ import {
   CardTitle,
 } from "../../../components/styles/Card/index.styled";
 import { useAppDispatch, useAppSelector } from "../../../store/hooks";
-import { getMovies, moviesSelector } from "../../../features/movie/slice";
+import movieSlice, {
+  getMovies,
+  moviesSelector,
+} from "../../../features/movie/slice";
 
 const Home: React.FC = () => {
   const { movies } = useAppSelector(moviesSelector);
   const dispatch = useAppDispatch();
+  const { sortByReleaseDate } = movieSlice.actions;
 
   React.useEffect(() => {
     dispatch(getMovies());
   }, [dispatch]);
 
+  const handleSortByReleaseDate = () => {
+    console.log("Sort by release date: ");
+    // const sortedMovie = movies?.data?.sort((a, b) => {
+    //   if (a?.release_date > b?.release_date) {
+    //     return 1;
+    //   }
+
+    //   if (a?.release_date < b?.release_date) {
+    //     return -1;
+    //   }
+
+    //   return 0;
+    // });
+
+    // console.log(sortedMovie);
+  };
+
+  const handleSortByRating = () => {
+    console.log("Sort by rating");
+  };
+
+  console.log("New movies sorted: ", movies);
+
   return (
     <React.Fragment>
       <div className="container">
-        <ul>
-          {movies?.data?.map((m, idx) => {
-            return (
-              <CardContainer key={idx}>
-                <Card>
-                  <CardTitle>
-                    <Link to={`movies/${m?.id}`}>{m?.title}</Link>
-                  </CardTitle>
-                  <CardBody>
-                    <p>{m?.overview}</p>
-                  </CardBody>
-                </Card>
-              </CardContainer>
-            );
-          })}
-        </ul>
+        <div className="btn-group">
+          <button onClick={() => dispatch(sortByReleaseDate(movies))}>
+            Sort by release date
+          </button>
+          <button onClick={handleSortByRating}>Sort by rating</button>
+        </div>
+        {movies?.data?.map((m, idx) => {
+          return (
+            <CardContainer key={idx}>
+              <Card>
+                <CardTitle>
+                  <Link to={`movies/${m?.id}`}>{m?.title}</Link>
+                </CardTitle>
+                <CardBody>
+                  <p>{m?.overview}</p>
+                </CardBody>
+              </Card>
+            </CardContainer>
+          );
+        })}
       </div>
     </React.Fragment>
   );
