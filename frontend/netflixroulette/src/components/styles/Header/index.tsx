a1fee6c320fcb4b38ba6068a3510b928efa640fb
Richard Orofeo <richard_orofeo@epam.com>
2023-01-06 21:32:42 +0800

    added sass as style

---
+++ frontend/netflixroulette/src/components/styles/Header/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { AppBar, Toolbar } from "@mui/material";
+
+export const Header: React.FC = () => {
+  return (
+    <React.Fragment>
+      <header>
+        <AppBar>
+          <Toolbar>
+            <b>netflix</b>
+            roulette
+          </Toolbar>
+        </AppBar>
+      </header>
+    </React.Fragment>
+  );
+};
